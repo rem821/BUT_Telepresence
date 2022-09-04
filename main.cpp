6a00ccc1fd0010fb31e5cbe0ee18612f64b6c876
Aki-7 <aki.develop8128@gmail.com>
2022-09-04 15:52:47 +0900

    link openxr loader

--- main.cpp
+++ main.cpp
@@ -1,8 +1,49 @@
-#include <android_native_app_glue.h>
 #include <android/log.h>
+#include <android_native_app_glue.h>
+#include <openxr/openxr.h>
+#include <openxr/openxr_platform.h>
 
-#define LOG(...) __android_log_print(ANDROID_LOG_INFO, "oculus_openxr_poc", __VA_ARGS__)
+#include <exception>
+#include <memory>
+
+#define LOG_INFO(...) __android_log_print(ANDROID_LOG_INFO, "oculus_openxr_poc", __VA_ARGS__)
+#define LOG_ERROR(...) __android_log_print(ANDROID_LOG_ERROR, "oculus_openxr_poc", __VA_ARGS__)
 
 void android_main(struct android_app *app) {
-    LOG("Hello World");
+  try {
+    JNIEnv *Env;
+    app->activity->vm->AttachCurrentThread(&Env, nullptr);
+
+    PFN_xrInitializeLoaderKHR initializeLoader = nullptr;
+    if (XR_SUCCEEDED(xrGetInstanceProcAddr(XR_NULL_HANDLE,
+                                           "xrInitializeLoaderKHR",
+                                           (PFN_xrVoidFunction *) (&initializeLoader)))) {
+      XrLoaderInitInfoAndroidKHR loaderInitInfoAndroid;
+      memset(&loaderInitInfoAndroid, 0, sizeof(loaderInitInfoAndroid));
+      loaderInitInfoAndroid.type = XR_TYPE_LOADER_INIT_INFO_ANDROID_KHR;
+      loaderInitInfoAndroid.next = nullptr;
+      loaderInitInfoAndroid.applicationVM = app->activity->vm;
+      loaderInitInfoAndroid.applicationContext = app->activity->clazz;
+      initializeLoader((const XrLoaderInitInfoBaseHeaderKHR*)&loaderInitInfoAndroid);
+    }
+
+    while(true) {
+      for (;;) {
+        int events;
+        struct android_poll_source *source;
+
+        if (ALooper_pollAll(0, nullptr, &events, (void**)&source) < 0) {
+          break;
+        }
+
+        if (source != nullptr) {
+          source->process(app, source);
+        }
+      }
+    }
+  } catch (const std::exception &ex) {
+    LOG_ERROR("%s", ex.what());
+  } catch (...) {
+    LOG_ERROR("Unknown Error");
+  }
 }
