3009af5ddbd2b336ff1c0b14df05d17e5f06a173
standa <standa.svediroh@gmail.com>
2025-09-23 07:54:49 +0200

    add an option to disable logging

--- include/log.h
+++ include/log.h
@@ -2,3 +2,7 @@
 
 #define LOG_INFO(...) __android_log_print(ANDROID_LOG_INFO, "but_telepresence", __VA_ARGS__)
 #define LOG_ERROR(...) __android_log_print(ANDROID_LOG_ERROR, "but_telepresence", __VA_ARGS__)
+
+//NOOP
+//#define LOG_INFO(...)  do {} while(0)
+//#define LOG_ERROR(...) do {} while(0)
